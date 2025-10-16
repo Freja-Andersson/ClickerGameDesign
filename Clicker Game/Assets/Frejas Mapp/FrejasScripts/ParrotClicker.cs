712a761814789fc49a318e1a99b7de8db00a5670
Freja Andersson <freja.andersson08@gmail.com>
2025-10-16 08:22:42 +0200

    Can now click and move the birds

--- Clicker Game/Assets/Frejas Mapp/FrejasScripts/ParrotClicker.cs
+++ Clicker Game/Assets/Frejas Mapp/FrejasScripts/ParrotClicker.cs
@@ -1,24 +1,61 @@
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class ParrotClicker : MonoBehaviour
 {
+    bool holdingParrot = false;
 
-    bool canClick;
-
-    void OnTriggerEnter2D(Collider2D other)
+    void Update()
     {
-        if (other.CompareTag("Parrot"))
+        ClickOnParrot();
+
+        MoveParrot();
+    }
+
+    void ClickOnParrot()
+    {
+        // Clicks on the parrot
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("canClick becomes true");
-            canClick = true;
+            Vector2 clickPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(clickPosition), Vector2.zero);
+            if (hit)
+            {
+                if (hit && hit.transform.gameObject.tag == "Parrot")
+                {
+                    Debug.Log("clicked on parrot");
+                }
+            }
+        }
+
+        // This in the method to check if the mouse holds the parrot
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector2 clickPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(clickPosition), Vector2.zero);
+            if (hit && hit.transform.gameObject.tag == "Parrot")
+            {
+                Debug.Log("Found object to move");
+                holdingParrot = true;
+            }
+        }
+        // if not holding down the mouse button you stop moving the parrot
+        if (Input.GetMouseButtonUp(1))
+        {
+            holdingParrot = false;
         }
     }
 
-    void OnMouseDown()
+    void MoveParrot()
     {
-        if (canClick)
+        if (!holdingParrot) { return; }
+
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        if (holdingParrot)
         {
-            Debug.Log("Parrot is clicked");
+            transform.position = Camera.main.ScreenToWorldPoint(mousePos);
         }
     }
+
 }
+
